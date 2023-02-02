74baf096866650063697ba9115dbd12f2981fe1d
mahdi-21 <83784541+mahdi-21@users.noreply.github.com>
2023-02-02 09:55:45 +0300

    string challenge

---
+++ 5-String_methods/string_methods.js
@@ -0,0 +1,21 @@
+let a = "Elzero Web School";
+
+// type Zero :
+
+console.log(a.slice(2, 7).charAt(0).toUpperCase() + a.slice(2, 7).slice(1));
+
+// HHHHHHHH:
+
+console.log(a.charAt(13).toUpperCase().repeat(8));
+
+// "Elzero":
+
+console.log(a.slice(0, 6).split(" "));
+
+// Elzero :
+
+console.log(a.substring(0, 6).toString());
+
+// Elzero school :
+
+console.log(a.substring(0, 6) + a.substring(10));
