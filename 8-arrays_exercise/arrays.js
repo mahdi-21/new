4428ff08e07b52a5fe8204009b1825c792125f7e
mahdi-21 <83784541+mahdi-21@users.noreply.github.com>
2023-02-03 16:38:48 +0300

    arrays example

---
+++ 8-arrays_exercise/arrays.js
@@ -0,0 +1,10 @@
+let a = ["mahdi", "fadi", "abdullah"];
+let b = ["sandouk", "alashhab", "najjar"];
+
+function sort() {
+  let c = [];
+  for (var i = 0; i < a.length; i++) c.push(a[i], b[i]);
+  console.log(c);
+}
+console.log(sort());
+console.log(sort);
