ce54645a2df861e9dcc3f52ad6618ad6ed1fc4e6
mahdi-21 <83784541+mahdi-21@users.noreply.github.com>
2023-02-07 13:46:38 +0300

    documented

--- 9.3-nested_loops/nested2.js
+++ 9.3-nested_loops/nested2.js
@@ -3,23 +3,16 @@ let sTeams = ["Real Madrid", "Barcelona", "Getafe"];
 let eTeams = ["Liverpool", "Chelsea", "Arsenal"];
 let iTeams = ["Ac Milan", "Inter Milan", "Atalanta"];
 
-console.log("Europe Leagues :");
+document.write(`<h1>The ${leagues.length} Leagues are :</h1>`);
 
 for (let i = 0; i < leagues.length; i++) {
-  console.log(`*** ${leagues[i]} ***`);
-  for (let j = 0; j < sTeams.length; j++) {
-    if (leagues[i] == "Spanish") {
-      console.log(`- ${sTeams[j]}`);
-    }
-  }
-  for (let k = 0; k < eTeams.length; k++) {
-    if (leagues[i] == "English") {
-      console.log(`- ${eTeams[k]}`);
-    }
-  }
-  for (let m = 0; m < iTeams.length; m++) {
-    if (leagues[i] == "Italian") {
-      console.log(`- ${iTeams[m]}`);
-    }
-  }
+  document.write(`<div>`);
+  document.write(`<h3>[${i + 1}] ${leagues[i]} </h3>`);
+  for (let j = 0; j < sTeams.length; j++)
+    if (leagues[i] == "Spanish") document.write(`<h4>- ${sTeams[j]}</h4>`);
+  for (let k = 0; k < eTeams.length; k++)
+    if (leagues[i] == "English") document.write(`<h4>- ${eTeams[k]}</h4>`);
+  for (let l = 0; l < iTeams.length; l++)
+    if (leagues[i] == "Italian") document.write(`<h4>- ${eTeams[l]}</h4>`);
+  document.write(`</div`);
 }
