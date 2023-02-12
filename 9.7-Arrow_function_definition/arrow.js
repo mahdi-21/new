d26fda7f44519bc5885f9c2f97a850a16a8f48ca
mahdi-21 <83784541+mahdi-21@users.noreply.github.com>
2023-02-12 11:25:12 +0300

    arrow function

---
+++ 9.7-Arrow_function_definition/arrow.js
@@ -0,0 +1,9 @@
+// Regular function :
+let print = function () {
+  console.log("Hello mahdi");
+};
+// Arrow function :                                  we put the () char if we had more than parameter,if we have only one we can put the _ char instead of ().
+let print2 = () => console.log("Hello Arrow"); // in one statement case , no need to the (return and {}).
+
+print();
+print2();
